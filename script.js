fc3ed3ab1638dc95d0d83597a6cfa3444ec8641b
xbdon <140739991+xbdon@users.noreply.github.com>
2024-06-08 23:02:23 -0400

    Create script.js
    Set up

---
+++ script.js
@@ -0,0 +1,5 @@
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
+const hamburger = document.getElementById("hamburger");
+const blastPrice = document.getElementById("blast-price");
+const tokenRow = document.getElementById("token-rows");
