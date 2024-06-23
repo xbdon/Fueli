e16c26806159c3d59951aa7444a1242f3e599e63
xbdon <140739991+xbdon@users.noreply.github.com>
2024-06-22 23:16:00 -0400

    Fixed Both API's and now displaying both price and supply of coin to DOM
    -found out how to use headers for fetch calls
    -used header knowledge to fix CG API call
    -added both API calls to fetchData()
    -configured displayTokenData() to take two parameters of json objects
    -fixed contractAddress variable bug where accessing object keys was returning undefined * json.contractAddress notation to json[contractAddress] notation
    -displayed both price and supply to DOM through displayTokenData()

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const supplyApi = `https://api.blastscan.io/api?module=stats&action=tokensupply&contractaddress=0x5ffd9EbD27f2fcAB044c0f0a26A45Cb62fa29c06&apikey=$$$$$$$$$`;
-const priceApi = `https://api.coingecko.com/api/v3/simple/token_price/blast?id?contract_addresses=0x5ffd9EbD27f2fcAB044c0f0a26A45Cb62fa29c06&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true`;
+const supplyApi = `https://api.blastscan.io/api?module=stats&action=tokensupply&contractaddress=0x5ffd9EbD27f2fcAB044c0f0a26A45Cb62fa29c06&apikey=$$$$$$$`;
+const priceApi = `https://api.coingecko.com/api/v3/simple/token_price/blast?contract_addresses=0x5ffd9EbD27f2fcAB044c0f0a26A45Cb62fa29c06&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true`;
 const searchBtn = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
 const hamburger = document.getElementById("hamburger");
@@ -35,35 +35,44 @@ const search = (data) => {
   }
 };
 
-// figure out how to set headers for responses *for api-key of coingecko api
 const fetchData = async () => {
   try {
-    const response = await fetch(priceApi);
-    const data = await response.json();
-    // const resSupply = await fetch(supplyApi);
-    // const dataSupply = await resSupply.json();
-    // const resPrice = await fetch(priceApi);
-    // const dataInfo = await resPrice.json();
-    // add all api calls to fetch data function
-    displayTokenData(data);
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "$$$$$$$$$",
+      },
+    };
+
+    const jsonPrice = await fetch(
+      "https://api.coingecko.com/api/v3/simple/token_price/blast?contract_addresses=0x5ffd9EbD27f2fcAB044c0f0a26A45Cb62fa29c06&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true",
+      options
+    );
+    const resPrice = await jsonPrice.json();
+
+    const jsonSupply = await fetch(supplyApi);
+    const resSupply = await jsonSupply.json();
+    console.log(resPrice + "Hello darkness my old friend");
+    displayTokenData(resPrice, resSupply);
   } catch (err) {
     console.log(err + " trouble obtaining data from api");
   }
 };
 
-// add another parameter to displayTokenData function for supply data
-const displayTokenData = (json) => {
+const displayTokenData = (json, supply) => {
   const contractAddress = Object.keys(json)[0];
+  const { result } = supply;
 
   tokenStats.innerHTML = `
     <tr>
       <td id="token-name" class="stats">${1}</td>
-      <td id="price" class="stats">${json.contractAddress}</td>
+      <td id="price" class="stats">${json[contractAddress].usd}</td>
       <td id="mc" class="stats">${1}</td>
       <td id="24" class="stats">${1}</td>
       <td id="volume" class="stats">${1}</td>
       <td id="liquidity" class="stats">${1}</td>
-      <td id="circ-supply" class="stats">${1}</td>
+      <td id="circ-supply" class="stats">${result}</td>
     </tr>
   `;
 };
@@ -77,5 +86,6 @@ const showOutput = () => {
 
 searchBtn.addEventListener("click", showOutput);
 fetchData();
+
 
 
