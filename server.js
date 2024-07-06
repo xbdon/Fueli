14e91ee4147c09c1525aeb1af4ba5542f30659c5
xbdon <140739991+xbdon@users.noreply.github.com>
2024-07-06 14:40:29 -0400

    Change to import syntax

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(process.cwd() + "/public")));
 
 app.use((req, res) => {
   res.status(404);
@@ -11,5 +11,6 @@ app.use((req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("App listening on port 3000");
+  console.log("Server listening on port 3000");
 });
+
