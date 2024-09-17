0ece889cbac66cf8265c9ca6bbd23a3cfb4f9f1d
xbdon <140739991+xbdon@users.noreply.github.com>
2024-09-17 15:37:56 -0400

    reverting file to use require module and new backend
    -changing from es6 modules to require module (when backend skills improve will go back to es6)
    -implementing new back-end knowledge to create a more robust beginner back-end server
    -front-end now connects to back-end, utilizing new API, axios, controllers and routes

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const coinData = require('./routes/coin-data');
+
+app.use((express.static("./public")))
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api/coin-data/get', coinData);
+
+app.listen(3000, () => {
+  console.log("It's alive HAUHAHUAHUAHUA!!!!")
+})
