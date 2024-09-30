1a5424ee1c33af457d534862711ed9d35e526a62
Branddon Echeverria <bdon452@gmail.com>
2024-09-30 18:07:22 -0400

    rounding the supply and price values to their designated target place values

--- public/index.js
+++ public/index.js
@@ -13,11 +13,7 @@ const tokenStats = document.getElementById("token-stats");
 if no coin is found from search input,
 then value changes to false*/
 let fetchDataResult = true;
-// Regex for cutting prices to two decimal points
-const regex = `/^(?!^0\.00$)(([1-9][\d]{0,6})|([0]))\.[\d]{2}$/`;
 
-// Either searchToken() or fetchData() will replace eachother: 
-// this is a note for tomorrow's work
 const searchToken = async () => {
   try {
     console.log(searchInput.value);
@@ -58,11 +54,11 @@ const displayTokenData = (json) => {
   fetchDataResult = true;
 
   const name = json.ticker;
-  const price = json.price;
-  const marketCap = json.mcap;
+  const price = Math.round(json.price * 1000000) / 1000000;
+  const marketCap = Math.round(json.mcap);
   // const twentyFourHr = json.market_data.price_change_percentage_24h;
   // const volume = json.market_data.total_volume.usd;
-  const circSupply = json.circSupply;
+  const circSupply = Math.round(json.circSupply);
 
   searchedTokenStats.innerHTML = `
     <tr>
