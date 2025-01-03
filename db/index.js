d7026e8eed1abca0dc63f11204547caecb053a25
Branddon Echeverria <bdon452@gmail.com>
2025-01-02 22:22:24 -0500

    investigating further into why client variable in db/index.js file is returning undefined, could be something to do with using express and file structure

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    username: "postgres",
+    user: "postgres",
     host: "localhost",
     database: "postgres",
     password: process.env.SQL_KEY,
@@ -13,12 +13,11 @@ const pool = new Pool({
     allowExitOnIdle: false
 });
 
-(async () => {
-    const client = await pool.connect();
-    // pool.idleCount;
-    // pool.totalCount;
-    // pool.waitingCount;
+const getDatabase = async () => {
+    const client = await pool.connect(process.env.DATABASE_URL);
     const { rows } = await client.query('SELECT * FROM coins')
     console.log(rows);
     client.release(true);
-})();
+}
+
+getDatabase();
