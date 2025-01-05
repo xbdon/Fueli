c7ef991e5656fe8674ce150703dd2ee0763d7da6
Branddon Echeverria <bdon452@gmail.com>
2025-01-05 17:54:23 -0500

    adding the the createUser function on the front end

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const coinData = require('./routes/coin-data');
 const searchToken = require('./routes/search-token');
 const adaPrice = require('./routes/ada-price');
 const dbFetch = require('./routes/db-route');
+const createUser = require('./routes/create-user');
 
 app.use((express.static("./public")))
 
@@ -14,7 +15,15 @@ app.use('/api/coin-data/get', coinData);
 app.use('/api/search-coin/get/', searchToken);
 app.use('/api/ada-price/get', adaPrice);
 app.use('/api/db-fetch/get', dbFetch);
+app.use('/api/create-user/post', createUser);
 
 app.listen(3000, () => {
   console.log("It's alive HAUHAHUAHUAHUA!!!!")
 })
+
+// in the future this object will be added to a database but for
+// learning purposes atm
+
+let userData = {
+  user: []
+}
--- controllers/user-functions.js
+++ controllers/user-functions.js
@@ -4,10 +4,12 @@ const db = require('../db');
 
 const createUser = (request, response) => {
     const newEntry = request.body;
-    console.log(newEntry);
+    console.log(`We made it to createUser function. And here is the data: ${newEntry}`);
     userData.push(newEntry.user);
     response.sendStatus(201);
 }
+
+createUser();
 
 module.exports = {
     createUser
