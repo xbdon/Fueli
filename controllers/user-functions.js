7c1258b3d58ee0164ead37fa5d2d156bff9a2394
Branddon Echeverria <bdon452@gmail.com>
2025-01-17 10:47:50 -0500

    starting new solution to make post method finally work

--- controllers/user-functions.js
+++ controllers/user-functions.js
@@ -1,18 +1,17 @@
-require("dotenv").config();
+
 const axios = require("axios");
 const db = require('../db');
 
 const createUser = (request, response) => {
-    console.log("made it to user-function controllers!")
-    const { data } = request.body;
-    console.log(data);
-    if (!data) {
-        return response.status(400).send({ status: 'failed' });
+    console.log(request.body);
+    try {
+        console.log("made it to user-function controllers!")
+        const { data } = request.body;
+        console.log(data);
+        return response.sendStatus(200).send({ status: 'recieved' });
+    } catch (err) {
+        response.status(400).send({ status: 'failed' });
     }
-    console.log(`We made it to createUser function. And here is the data: ${data}`);
-    // userData.push(accData.user);
-    console.log(data);
-    return response.sendStatus(200).send({ status: 'recieved' });
 }
 
 module.exports = {
