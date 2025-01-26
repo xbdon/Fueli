73cf5272641600313df01be6d7f1e53451c03932
Branddon Echeverria <bdon452@gmail.com>
2025-01-26 12:50:36 -0500

    added new try block to fix to asses if data was sent to database

--- src/controllers/user-functions.js
+++ src/controllers/user-functions.js
@@ -9,10 +9,14 @@ const createUser = (req, res) => {
     const { email, username, password, cPassword } = req.body;
     // save username and irreversibly encrypt password
 
-    if (password === cPassword) {
-        const hashedPassword = bcrypt.hashSync(password, 8)
+    const hashedPassword = bcrypt.hashSync(password, 8)
 
-        console.log(hashedPassword);
+    // save the new user and hashed password to the db
+    try {
+        console.log("Your Fueli account was created!");
+    } catch (err) {
+        console.log(err.message);
+        res.sendStatus(503)
     }
 
     console.log("Your Fueli account was created!");
@@ -21,8 +25,8 @@ const createUser = (req, res) => {
 
 const login = (req, res) => {
     // we get their email, and we look up the password associated with that email in the db 
-    // if we compare the encrypted password with password input of login it wont match
-    // we must encrypt the password input and then compare it to db on file
+    //  if we compare the encrypted password with password input of login it wont match
+    //  we must encrypt the password input and then compare it to db on file
 
 }
 
