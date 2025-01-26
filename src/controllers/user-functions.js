11492df870f786a2843868b0450f575a9fe6eb08
Branddon Echeverria <bdon452@gmail.com>
2025-01-26 11:50:02 -0500

    cleaned up createUser controller function. Try catch block not needed, took away send method causes error.

--- src/controllers/user-functions.js
+++ src/controllers/user-functions.js
@@ -1,6 +1,8 @@
 // const axios = require("axios");
 // const db = require('../db');
 import axios from 'axios';
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import db from '../db.js'
 
 const createUser = (req, res) => {
@@ -8,13 +10,12 @@ const createUser = (req, res) => {
     console.log("XxX")
     console.log(email, username, password, cPassword)
 
-    try {
-        console.log("Your Fueli account was created!");
-        // return res.sendStatus(200).send({ status: 'recieved' });
-    } catch (err) {
-        console.log(err)
-        res.status(400).send({ status: 'failed' });
-    }
+    console.log("Your Fueli account was created!");
+    res.sendStatus(201);
+}
+
+const login = (req, res) => {
+
 }
 
 export default createUser;
