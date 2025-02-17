fff3a5d50a4da85687e12b091e5049ae86ec685d
Branddon Echeverria <bdon452@gmail.com>
2025-02-17 09:20:51 -0500

    added controllers and routes for isCoinSaved()

--- src/routes/user-functions.js
+++ src/routes/user-functions.js
@@ -2,7 +2,7 @@ import express from 'express';
 import db from '../db.js'
 const router = express.Router();
 
-import { saveCoin, getWatchlist, deleteCoin } from '../controllers/user-functions.js';
+import { saveCoin, getWatchlist, deleteCoin, checkCoin } from '../controllers/user-functions.js';
 
 // gets watchlist to display on front-end
 router.get('/getWatchlist', getWatchlist);
@@ -12,5 +12,8 @@ router.post('/save-coin', saveCoin);
 
 // deletes coins from watchlist db
 router.delete('/delete-coin/:ticker/:coinId', deleteCoin);
+
+// checks if coin is already in watchlist
+router.get('/check-coin', checkCoin);
 
 export default router;
