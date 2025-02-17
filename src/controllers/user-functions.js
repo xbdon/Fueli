2b27fae6833fe8c7ab6cf83c00bd1bbc202f6a17
Branddon Echeverria <bdon452@gmail.com>
2025-02-17 12:39:51 -0500

    added coin_exists to make return value easy to parse

--- src/controllers/user-functions.js
+++ src/controllers/user-functions.js
@@ -108,7 +108,7 @@ const deleteCoin = (req, res) => {
 const checkCoin = (req, res) => {
     const { coinId } = req.params
     const userId = req.userId
-    const checkCoin = db.prepare(`SELECT EXISTS(SELECT 1 FROM watchlist WHERE user_id = ? AND coin_id = ?)`)
+    const checkCoin = db.prepare(`SELECT EXISTS(SELECT 1 FROM watchlist WHERE user_id = ? AND coin_id = ?) AS coin_exists`)
     const bool = checkCoin.get(userId, coinId)
     console.log(bool)
     console.log(`this coin is in the watchlist: ${bool.changes}`);
