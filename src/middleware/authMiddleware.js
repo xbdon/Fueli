1a996ed9a811c4f9a4a4f9b82b8a08b47710f2a4
Branddon Echeverria <bdon452@gmail.com>
2025-02-28 09:26:45 -0500

    added a class that changes background color. this will be used for when one of the rank buttons are clicked to show that it is activated already

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken'
 
 function authMiddleware(req, res, next) {
-    console.log("made it to authMiddleware")
     const token = req.headers['authorization']
 
     // undefined token edge case was already put in front-end but this step will be added regardless
